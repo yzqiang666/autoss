8918eade1f663d63005740620934a54fbb07d18f
yzqiang666 <yzqiang6@sina.com>
2017-04-16 06:49:01 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -3,12 +3,21 @@ wget -q -O jq --no-check-certificate  https://raw.github.com/yzqiang666/autoss/m
 wget -q -O sscfg.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/sscfg.sh
 wget -q -O getss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/getss.sh
 
+wget -q -O setss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/setss.sh
+
+
+
+
+
 cp /opt/bin/jq1 jq
 #cp /opt/bin/jq /tmp
 #cp /opt/bin/sscfg.sh /tmp
 #cp /opt/bin/getss.sh /tmp
 chmod a+x sscfg.sh
 chmod a+x getss.sh
+chmod a+x setss.sh
 chmod a+x jq
 ./sscfg.sh
+./setss.sh
+
 
