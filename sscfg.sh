c8b367300a5e19e3561d99f18cd2dcbf20bc5d90
yzqiang666 <yzqiang6@sina.com>
2017-04-16 07:00:58 +0800

    Update sscfg.sh

--- sscfg.sh
+++ sscfg.sh
@@ -5,5 +5,5 @@ getss.sh e39ed54e-18ee-4eae-b372-41b4e05721f3:eoZ9cCkTpM0d6Rb7BEtXl5luBcqZyVeiNL
 getss.sh e39ed54e-18ee-4eae-b372-41b4e05721f3:eoZ9cCkTpM0d6Rb7BEtXl5luBcqZyVeiNLZuKUxGjgOFnB1tqTChz3Wr8JKS2kJY dreamy-stonebraker-6002.arukascloud.io 8888 yzqyzq rc4-md5 auth_sha1 http_simple >>ss.ini
 getss.sh e39ed54e-18ee-4eae-b372-41b4e05721f3:eoZ9cCkTpM0d6Rb7BEtXl5luBcqZyVeiNLZuKUxGjgOFnB1tqTChz3Wr8JKS2kJY silly-keller-4692.arukascloud.io 8080 yzqyzq rc4-md5 >>ss.ini
 
-rnd=`awk 'BEGIN{srand();print int(rand()*10)+1 }'`
+#rnd=`awk 'BEGIN{srand();print int(rand()*10)+1 }'`
 #echo $rnd >>ss.ini
