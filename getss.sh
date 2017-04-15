44b8b61c6b538852c637119f6adc1d42da24978b
yzqiang666 <yzqiang6@sina.com>
2017-04-16 00:07:55 +0800

    Update getss.sh

--- getss.sh
+++ getss.sh
@@ -8,7 +8,7 @@ fi
 
 #wget -O ss.txt -q https://e39ed54e-18ee-4eae-b372-41b4e05721f3:eoZ9cCkTpM0d6Rb7BEtXl5luBcqZyVeiNLZuKUxGjgOFnB1tqTChz3Wr8JKS2kJY@app.arukas.io/api/containers
 rawJson=`/tmp/jq '.data' <ss.txt`
-length=`echo $rawJson | /tmp/jq "length"`
+length=`echo $rawJson | jq "lengt
 let "length-=1"
 addr="lost"
 port="0"
@@ -16,18 +16,18 @@ aimAddr="\"$2\""
 aimPort=$3
 port_rules="port=$4"
 for i in $(seq 0 $length) ; do
-	endP=`echo $rawJson | /tmp/jq ".[$i].attributes.end_point"`
+	endP=`echo $rawJson | jq ".[$i].attributes.end_point"`
         for k in $(seq 0 9) ; do
 	if [ "$endP" = "$aimAddr" ] ; then
 		portMapping=`echo $rawJson | /tmp/jq ".[$i].attributes.port_mappings|.[$k]"`
-		portMappingLength=`echo $portMapping | /tmp/jq "length"`
+		portMappingLength=`echo $portMapping | jq "length"`
                 let "portMappingLength-=1"
 		for j in $(seq 0 $portMappingLength) ; do
-			cPortJson=`echo $portMapping | /tmp/jq ".[$j]"`
+			cPortJson=`echo $portMapping |  ".[$j]"`
 			cPort=`echo $cPortJson | jq ".container_port"`
 			if [ "$cPort" = "$aimPort" ] ; then
-				port=`echo $cPortJson | /tmp/jq ".service_port"`
-				addr=`echo $cPortJson | /tmp/jq ".host" | awk -F '"' '{printf $2}'`
+				port=`echo $cPortJson | jq ".service_port"`
+				addr=`echo $cPortJson | jq ".host" | awk -F '"' '{printf $2}'`
                                 echo $addr | awk -F"[-.]" '{print $2"."$3"."$4"."$5; }' >tmp.txt 
                                 addr=`cat tmp.txt`
                                 echo "$addr:$port:$4:$5:$6:$7"
