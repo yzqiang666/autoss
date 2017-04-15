03e95144af8c0f4ad4440aa89f854595272306fc
yzqiang666 <yzqiang6@sina.com>
2017-04-16 06:56:05 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -1,3 +1,12 @@
+
+ss_internet=`nvram get ss_internet`
+ss_enable=`nvram get ss_enable`
+
+if [ "$ss_enable" == "0" ] ; then
+logger  "SS is disable"
+elif [ "$ss_internet" == "1" ]; then
+logger "SS Status is working"
+else
 cd /tmp
 wget -q -O jq --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/jq
 wget -q -O sscfg.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/sscfg.sh
@@ -19,5 +28,7 @@ chmod a+x setss.sh
 chmod a+x jq
 ./sscfg.sh
 ./setss.sh
+
+fi
 
 
