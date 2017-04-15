4e6e54c602c7e4b4b94b8d718826d3c5f734909b
yzqiang666 <yzqiang6@sina.com>
2017-04-15 23:56:32 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -1,3 +1,14 @@
-logger "begin auto ss"
-date >>/tmp/ssr.log
+cd /tmp
+wget -q -O jq --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/jq
+wget -q -O /sscfg.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/sscfg.sh
+wget -q -O getss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/getss.sh
+
+
+#cp /opt/bin/jq /tmp
+#cp /opt/bin/sscfg.sh /tmp
+#cp /opt/bin/getss.sh /tmp
+chmod a+x sscfg.sh
+chmod a+x getss.sh
+chmod a+x jq
+./sscfg.sh
 
