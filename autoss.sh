ebb85efecd90197c830ce52d6f5f63e15e3deb9f
yzqiang666 <yzqiang6@sina.com>
2017-04-16 09:44:16 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -7,29 +7,32 @@ logger  "SS is disable"
 elif [ "$ss_internet" == "1" ]; then
 logger "SS Status is working"
 else
+
 cd /tmp
-wget -q -O jq --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/jq
-wget -q -O sscfg.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/sscfg.sh
-wget -q -O getss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/getss.sh
+#wget -q -O jq.tar.gz --no-check-certificate  https://github.com/yzqiang666/autoss/releases/download/jq/jq.tar.gz
+#wget -q -O sscfg.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/sscfg.sh
+#wget -q -O getss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/getss.sh
 
+wget -q -O ss.ini ftp://ftp:ftp/202.109.226.26/AiCard_02/ftp/ss.ini
 wget -q -O setss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/setss.sh
-wget -q -O jq ftp://ftp:ftp@202.109.226.26/AiCard_02/ftp/jq
-oldpath＝$PATH
-export PATH＝$oldpath:/tmp
+#wget -q -O jq ftp://ftp:ftp@202.109.226.26/AiCard_02/ftp/jq
+#oldpath＝$PATH
+#export PATH＝$oldpath:/tmp
 
 
+#tar xvzf jq.tar.gz
 
-cp /opt/bin/jq1 jq
+#cp /opt/bin/jq1 jq
 #cp /opt/bin/jq /tmp
 #cp /opt/bin/sscfg.sh /tmp
 #cp /opt/bin/getss.sh /tmp
-chmod a+x sscfg.sh
-chmod a+x getss.sh
+#chmod a+x sscfg.sh
+#chmod a+x getss.sh
 chmod a+x setss.sh
-chmod a+x jq
-./sscfg.sh
+#chmod a+x jq
+#./sscfg.sh
 ./setss.sh
-export PATH＝$oldpath
+#export PATH＝$oldpath
 
 fi
 
