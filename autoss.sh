9949ee82aefa4d28c7a88dee40e0ad56f6693f5f
yzqiang666 <yzqiang6@sina.com>
2017-04-16 10:48:53 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -9,6 +9,8 @@ logger "SS Status is working"
 else
 
 cd /tmp
+sh ss.sh stop                       
+sleep 5   
 #wget -q -O jq.tar.gz --no-check-certificate  https://github.com/yzqiang666/autoss/releases/download/jq/jq.tar.gz
 #wget -q -O sscfg.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/sscfg.sh
 #wget -q -O getss.sh --no-check-certificate  https://raw.github.com/yzqiang666/autoss/master/getss.sh
@@ -33,7 +35,7 @@ chmod a+x setss.sh
 #./sscfg.sh
 ./setss.sh
 #export PATH=$oldpath
-
+sh ss.sh start
 fi
 
 
