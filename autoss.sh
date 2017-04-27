eb9da37eb42899e9c06d6baf8a1d4271849489ae
yzqiang666 <yzqiang6@sina.com>
2017-04-27 22:13:16 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -9,8 +9,8 @@ cd /tmp
 nvram set ss_status=1
 nvram set ss_enable=0
 nvram commit
-pidof ss-redir  >/dev/null 2>&1 && killall ss-redir ss-local Sh15_ss.sh wgetcurl.sh curl ss-rules sh_sskeey_k.sh && killall -9ss-redir ss-local Sh15_ss.sh wgetcurl.sh curl ss-rules sh_sskeey_k.sh 2>/dev/null
-/etc/storage/script/Sh15_ss.sh stop &
+pidof ss-redir  >/dev/null 2>&1 && killall ss-redir && killall -9 ss-redir 2>/dev/null
+/etc/storage/script/Sh15_ss.sh stop 
 ss-rules -f
 sleep 10
 
@@ -254,11 +254,11 @@ fi
 ss-rules -f
 nvram set ss_enable=1
 
-pidof ss-redir  >/dev/null 2>&1 && killall ss-redir ss-local Sh15_ss.sh wgetcurl.sh curl ss-rules sh_sskeey_k.sh && killall -9ss-redir ss-local Sh15_ss.sh wgetcurl.sh curl ss-rules sh_sskeey_k.sh 2>/dev/null
+pidof ss-redir  >/dev/null 2>&1 && killall ss-redir  && killall -9 ss-redir 2>/dev/null
 sleep 2
 nvram set ss_status=0
 nvram set ss_enable=1
 nvram commit
-/etc/storage/script/Sh15_ss.sh start &
+/etc/storage/script/Sh15_ss.sh start 
 
 
