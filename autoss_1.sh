a3cdbafedc2d72c3f8d0e63924a29abf430bf713
yzqiang666 <yzqiang6@sina.com>
2017-05-02 19:53:23 +0800

    Update autoss_1.sh

--- autoss_1.sh
+++ autoss_1.sh
@@ -6,7 +6,7 @@
 &&  /opt/shadowsocksr-manyuser/shadowsocks/run.sh
 
 #########################################
-url="http://www.youtube.com/"
+url="http://www.youtube.com"
 
 
 if [ ! "$1" = "refresh" ] ; then
@@ -200,11 +200,11 @@ cat ss.ini | while read str
 do
 #echo "begin process ===========   "$str
 if [ "$str" = "==========" ] ; then
-	if [ $CC -ge 10 ] ; then
-		break
-	else
-		continue
-	fi
+        if [ $CC -ge 10 ] ; then
+                break
+        else
+                continue
+        fi
 fi
 ss_s1_ip=`echo $str|awk -F ':' '{print $1}'`  
 ss_s1_port=`echo $str|awk -F ':' '{print $2}'`  
@@ -246,7 +246,7 @@ sleep 1
 #########正式测试时间
 starttime=$(cat /proc/uptime | cut -d" " -f1)
 rm /tmp/tmp.txt 2>/dev/null
-wget  -q -O /tmp/tmp.txt --continue --no-check-certificate   -T 15  $url 2>/dev/null
+wget  -q -O /tmp/tmp.txt --continue --no-check-certificate   -T 15  http://www.youtube.com/ 2>/dev/null
 #wget  -q -O /tmp/tmp.txt --continue --no-check-certificate   -T 3  http://www.google.co.jp/ 2>/dev/null
 if [ -s /tmp/tmp.txt ] ; then
         endtime=$(cat /proc/uptime | cut -d" " -f1)
@@ -274,8 +274,8 @@ if [ -s /tmp/tmp.txt ] ; then
     fi
     echo $str" =====  "$TIME $min $CC
     logger $str" =====  "$TIME $min
-	RES=`awk -v a=$TIME  'BEGIN { print (a<=15)?1:0'}`
-	[ "$RES" = "1"  ] && let CC=$CC+1
+        RES=`awk -v a=$TIME  'BEGIN { print (a<=15)?1:0'}`
+        [ "$RES" = "1"  ] && let CC=$CC+1
 else
     echo $str" =====  Fail" $min
     logger $str" =====  Fail" $min
@@ -336,5 +336,4 @@ watchdog
 restart_ss
 
 fi
-
 
