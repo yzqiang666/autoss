7199febc61019534dac82cfab3c467b17ea60a53
yzqiang666 <yzqiang6@sina.com>
2017-05-19 13:24:51 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -156,10 +156,9 @@ get_from_arukas
 get_from_ishadowsock
 get_from_other
 get_from_Alvin9999
-
+[ ! -s ss.ini ] && exit 1
 
 ###################### set ss information ####################################
-if [ -s ss.ini ] ; then
 logger "get bestss server"
 
 options1=""
@@ -314,4 +313,4 @@ nvram set ss_enable=1
 nvram commit
 /etc/storage/script/Sh15_ss.sh start >/dev/null  2>/dev/null &
 
-fi
+
