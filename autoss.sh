40c0ca322c944532c57ee27bc06cffb097493038
yzqiang666 <yzqiang6@sina.com>
2017-06-27 23:50:33 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -249,10 +249,11 @@ echo "==========" >> ss.ini
 cd /tmp
 rm ss.ini >/dev/null 2>&1
 sleep 1
+get_from_Alvin9999
 get_from_arukas
 get_from_ishadowsock
 get_from_other
-get_from_Alvin9999
+
 [ ! -s ss.ini ] && exit 1
 
 ###################### set ss information ####################################
