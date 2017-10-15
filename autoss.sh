8b8bf37d454a5b4710436e769903e476cb618146
yzqiang666 <yzqiang6@sina.com>
2017-10-15 16:31:14 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -14,7 +14,7 @@ rm /tmp/tmp.txt 2>/dev/null
 wget  -q  -O /tmp/tmp.txt  --no-check-certificate   -T 4 $url 2>/dev/null 
 [ ! -s /tmp/tmp.txt ] && wget  -q  -O /tmp/tmp.txt  --no-check-certificate   -T 4 https://www.google.com.hk 2>/dev/null 
 [ ! -s /tmp/tmp.txt ] && wget  -q  -O /tmp/tmp.txt  --no-check-certificate   -T 4 https://www.google.com.hk 2>/dev/null 
-###[ -s /tmp/tmp.txt ] && exit 0
+[ -s /tmp/tmp.txt ] && exit 0
 fi
 
 
