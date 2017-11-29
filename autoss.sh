7813117994243053ac0cd9e1a41e8721ecbec78b
yzqiang666 <yzqiang6@sina.com>
2017-11-29 10:21:25 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -378,6 +378,8 @@ cd /tmp
 rm ss.ini >/dev/null 2>&1
 sleep 1
 ssr_url="`nvram get ssr_url`"
+DNS="`nvram get ss_DNS_Redirect`"
+[ "$DNS" = "1" ] && nvram set ss_DNS_Redirect=0 && nvram commit
 get_from_tckssr
 [ ! -s ss.ini ] && curl $ssr_url"ss.ini" -o ss.ini
 get_from_ishadowsock
