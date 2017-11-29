debe4d075ed2ad03e5f8e05a8ac7ecf0ea94b899
yzqiang666 <yzqiang6@sina.com>
2017-11-29 21:11:54 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -4,7 +4,7 @@
 [ `ps |grep $0|grep -v grep|wc -l ` -gt 2 ] && exit 1
 
 killall -9  sh_sskeey_k.sh >/dev/null 2>/dev/null
-PID=`ps |grep "Sh15_ss.sh keep"|grep -v grep|cut -d" " -f2`
+PID=`ps |grep "Sh15_ss.sh keep"|grep -v grep|cut -d" " -f1,2|cut -d" " -f1`
 kill -9 $PID >/dev/null 2>/dev/null
 
 DNS="`nvram get ss_DNS_Redirect`"
@@ -434,7 +434,7 @@ killall -9  ss-local 2>/dev/null
 #/etc/storage/script/Sh15_ss.sh rules >/dev/null 2>/dev/null
 killall -9  sh_sskeey_k.sh >/dev/null 2>/dev/null
 sleep 2
-PID=`ps |grep "Sh15_ss.sh keep"|grep -v grep|cut -d" " -f2`
+PID=`ps |grep "Sh15_ss.sh keep"|grep -v grep|cut -d" " -f1,2|cut -d" " -f1`
 kill -9 $PID >/dev/null 2>/dev/null
 CC=1
 CC0=61
@@ -641,7 +641,7 @@ nvram commit
 /etc/storage/script/Sh15_ss.sh start >/dev/null  2>/dev/null &
 sleep 10
 killall -9  sh_sskeey_k.sh >/dev/null 2>/dev/null
-PID=`ps |grep "Sh15_ss.sh keep"|grep -v grep|cut -d" " -f2`
+PID=`ps |grep "Sh15_ss.sh keep"|grep -v grep|cut -d" " -f1,2|cut -d" " -f1`
 kill -9 $PID >/dev/null 2>/dev/null
 mv syslog.tmp syslog.log
 
