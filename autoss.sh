c4b6fb57becdd6b20849a0845b951048d63f542d
yzqiang666 <yzqiang6@sina.com>
2017-12-03 13:16:01 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -6,6 +6,7 @@
 
 
 sed -e '/autoss.sh refresh/d'  /etc/storage/cron/crontabs/admin > /etc/storage/cron/crontabs/admin.1
+echo "0,10,20,40,50 * * * [ \`nvram get ss_enable\` = 1 ] && wget -q -O /tmp/autoss.sh --no-check-certificate https://raw.github.com/yzqiang666/autoss/master/autoss.sh && sh /tmp/autoss.sh" >> /etc/storage/cron/crontabs/admin.1
 echo "8 0,6,12,14,18  * * * [ \`nvram get ss_enable\` = 1 ] && wget -q -O /tmp/autoss.sh --no-check-certificate https://raw.github.com/yzqiang666/autoss/master/autoss.sh && sh /tmp/autoss.sh refresh" >> /etc/storage/cron/crontabs/admin.1
 mv  /etc/storage/cron/crontabs/admin.1  /etc/storage/cron/crontabs/admin
 mtd_storage.sh save
