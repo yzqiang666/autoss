7cc4a15b9fec04a5d9a9b8d736460504b367ad82
yzqiang666 <yzqiang6@sina.com>
2018-02-01 10:37:08 +0800

    Create jx.js

---
+++ jx.js
@@ -0,0 +1,5 @@
+var jx_str0=["+|iqiyi|youku|https://jiexi.071811.cc/jx.php?url=",
+             "+|youku|http://jx.vgoodapi.com/jx.php?url=",
+             "-|iqiyi|http://www.wmxz.wang/video.php?url=",
+			 "-|youku|http://aikan-tv.com/?url=",
+			 "-|http://jx.vgoodapi.com/jx.php?url="]; 
