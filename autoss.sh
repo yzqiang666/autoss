fd4a64a042360474a24caee907ffbc2e69384924
yzqiang666 <yzqiang6@sina.com>
2018-03-18 07:42:13 +0800

    Update autoss.sh

--- autoss.sh
+++ autoss.sh
@@ -3,7 +3,7 @@
 [ `ps |grep $0|grep -v grep|wc -l ` -gt 2 ] && exit 1
 [ -f /tmp/cron_ss.lock ]  && exit 1
 
-nvram set tkcssr="link/FCgzUG7KGaSQFpLm"
+nvram set tkcssr="link/S6v4iuNmjynywEZ0?is_ss=0"
 nvram set ssr_url=" -u ssftp:ftp ftp://202.109.226.26/AiCard_01/opt/www/default/"
 nvram commmit
 
@@ -150,7 +150,7 @@ rm ss.txt > /dev/null 2>&1
 tkcssr="`nvram get tkcssr`"
 if [ ! "$tkcssr"x = "x" ] || [ "1" = "1" ] ; then 
 iss="https://capsule.cf/"$tkcssr
-iss="https://capsule.cf/link/zdV3ynUZyEoBp5Pa?is_ss=0"
+iss="https://www.capsule.cf/link/S6v4iuNmjynywEZ0?is_ss=0"
 curl -o ss.txt -s -k -L   -m 10 $iss 2>/dev/null
 
 if [  "$?" = "0" ] ; then
